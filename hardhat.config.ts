18467cede526f477ab55e6849f171d2b79e258ae
OmarMWarraich <earthling986@gmail.com>
2025-01-27 02:27:40 +0500

    feat: Smart Contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,17 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
 
 export default config;
