15b3863e9c827d55039bf567333e3770c696b39e
OmarMWarraich <earthling986@gmail.com>
2025-01-29 00:29:19 +0500

    style: Card, Carousel

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { useAppKitAccount } from "@reown/appkit/react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "./ui/card";
 
 export const Account: React.FC = () => {
   const {
@@ -12,21 +20,112 @@ export const Account: React.FC = () => {
     allAccounts,
   } = useAppKitAccount();
   return (
-    <div>
-      <div>{isConnected ? "Connected" : "Not Connected"}</div>
-      <div>{address}</div>
-      <div>{caipAddress}</div>
-      <div>{status}</div>
-      <div>
-        {embeddedWalletInfo
-          ? JSON.stringify(embeddedWalletInfo)
-          : "No Wallet Info"}
-      </div>
-      <div>
-        {allAccounts.map((account, index) => (
-          <div key={index}>{JSON.stringify(account)}</div>
-        ))}
-      </div>
-    </div>
+    <Carousel className="w-full max-w-md mx-auto">
+      <CarouselContent className="-mt-1">
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <span className="text-sm font-semibold break-all">
+                  <i>
+                    <b>
+                      <h6>Connected</h6>
+                    </b>
+                  </i>{" "}
+                  :{" "}
+                  <span className="break-all">
+                    {" "}
+                    {isConnected ? "Connected" : "Not Connected"}
+                  </span>
+                </span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <span className="text-sm font-semibold">
+                  <i>
+                    <b>Address</b>
+                  </i>{" "}
+                  : <span className="break-all">{address}</span>
+                </span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <span className="text-sm font-semibold">
+                  <i>
+                    <b>CaipAddress</b>
+                  </i>{" "}
+                  : <span className="break-all">{caipAddress}</span>
+                </span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <span className="text-sm font-semibold">
+                  <i>Status</i> : <span className="break-all">{status}</span>
+                </span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <span className="text-sm font-semibold">
+                  <i>
+                    <b>Wallet Info</b>
+                  </i>
+                  <code>
+                    {embeddedWalletInfo
+                      ? JSON.stringify(embeddedWalletInfo, null, 2)
+                      : "No Wallet Info"}
+                  </code>
+                </span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+
+        <CarouselItem className="pl-1 md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <span className="text-sm font-semibold">
+                  <i>
+                    <b>Accounts</b>
+                  </i>{" "}
+                  :{" "}
+                  <span className="break-all">
+                    {allAccounts.map((account, index) => (
+                      <div key={index}>{JSON.stringify(account, null, 2)}</div>
+                    ))}
+                  </span>
+                </span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 };
